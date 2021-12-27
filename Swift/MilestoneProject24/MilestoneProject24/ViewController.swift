b64db08c30783caf3aa812bb2bf23b06531b3f29
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2021-12-27 12:04:39 +0100

    Project 24

---
+++ Swift/MilestoneProject24/MilestoneProject24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProject24
+//
+//  Created by Paul-Louis Renard on 27/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
