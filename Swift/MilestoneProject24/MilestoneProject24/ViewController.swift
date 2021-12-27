a6273d349d98ac62fa8c5aaf5aced9cdd9691c25
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2021-12-27 12:19:03 +0100

    Milestone 24

--- Swift/MilestoneProject24/MilestoneProject24/ViewController.swift
+++ Swift/MilestoneProject24/MilestoneProject24/ViewController.swift
@@ -17,3 +17,27 @@ class ViewController: UIViewController {
 
 }
 
+extension UIView {
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration,delay: 0, options: .curveEaseOut) {
+            self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
+
+extension Int {
+    func times(completion: () -> Void) {
+        guard self > 0 else { return }
+        for _ in 0..<self {
+            completion()
+        }
+    }
+}
+
+extension Array where Element: Comparable {
+    mutating func remove(item: Element){
+        if let index = firstIndex(of: item) {
+            self.remove(at: index)
+        }
+    }
+}
