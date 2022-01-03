7c9fb444f548a807ca0f4d67b549637e13b79021
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2022-01-03 17:18:57 +0100

    Checkpoint 7

--- SwiftUI/CheckPoints.playground/Contents.swift
+++ SwiftUI/CheckPoints.playground/Contents.swift
@@ -39,3 +39,93 @@ func findSquareRoot(of number: Int) throws -> Int {
     }
     throw SquareRootError.noRoot
 }
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let luckyArray = luckyNumbers.filter { !$0.isMultiple(of: 2)}.sorted().map {
+    "\($0) is a lucky number"
+}
+
+for elt in luckyArray {
+    print(elt)
+}
+
+struct Car {
+    let model: String
+    let numberOfSeats: Int
+    private(set) var currentGear: Int = 1
+    
+    init(model: String, numberOfSeats: Int) {
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+    }
+    
+    mutating func incrementGear() {
+        currentGear += 1
+        if currentGear > 10 {
+            currentGear = 10
+        }
+    }
+    
+    mutating func decrementGear() {
+        currentGear -= 1
+        if currentGear < 1 {
+            currentGear = 1
+        }
+    }
+}
+
+class Animal {
+    let legs: Int
+    
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+
+class Dog: Animal {
+    init() {
+        super.init(legs: 4)
+    }
+    
+    func speak() {
+        print("Whaouf")
+    }
+}
+
+class Corgi: Dog {
+    override func speak() {
+        print("Corgi Whaouf")
+    }
+}
+
+class Poodle: Dog {
+    override func speak() {
+        print("Poodle Waouf")
+    }
+}
+
+class Cat: Animal {
+    var isTame: Bool
+    
+    init(isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: 4)
+    }
+    
+    func speak() {
+        print("Miaou")
+    }
+}
+
+class Persian: Cat {
+    override func speak() {
+        print("Persian Miaou")
+    }
+}
+
+class Lion: Cat {
+    override func speak() {
+        print("Lion miaou")
+    }
+}
