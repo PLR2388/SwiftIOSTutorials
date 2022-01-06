b8a38efb291f3bdbe7495839322bf7c2efaf4c02
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2022-01-06 08:14:25 +0100

    Guess the flag

---
+++ SwiftUI/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Paul-Louis Renard on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
