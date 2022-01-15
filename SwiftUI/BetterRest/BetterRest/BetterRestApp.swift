6170abc63b792f7f49d70144d394b69ac44b5e11
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2022-01-15 06:54:08 +0100

    Better Rest

---
+++ SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Paul-Louis Renard on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
