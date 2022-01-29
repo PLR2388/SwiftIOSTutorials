696f08533d8f02b58e81e5b3db14767049015359
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2022-01-29 11:21:57 +0100

    WordScramble

---
+++ SwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Paul-Louis Renard on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
