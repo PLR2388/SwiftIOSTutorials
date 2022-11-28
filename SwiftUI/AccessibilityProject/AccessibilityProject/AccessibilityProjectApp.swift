9e532bc0c69f3b6816ab48ed99a183fadad47fb5
Paul-Louis Renard <paul-louis.renard@outlook.fr>
2022-11-28 08:58:10 +0100

    Save accessibility

---
+++ SwiftUI/AccessibilityProject/AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Paul-Louis Renard on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
