e16bbe8894cea876ae94db8442f97d45a76b7ff2
Paul-Louis <paul-louis.renard@outlook.fr>
2023-01-07 15:51:03 +0100

    Try challenge but not working

---
+++ SwiftUI/RememberPerson/RememberPerson/RememberPersonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberPersonApp.swift
+//  RememberPerson
+//
+//  Created by Paul-Louis Renard on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RememberPersonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
