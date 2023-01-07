bb8dc4703cd16627bbd51069285317c1d349750d
Paul-Louis <paul-louis.renard@outlook.fr>
2023-01-07 16:12:49 +0100

    Hot Prospect Day 1

---
+++ SwiftUI/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Paul-Louis Renard on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
